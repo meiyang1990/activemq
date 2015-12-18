e37bb2ec2d8615d6c41e04c0d7132b87406876b7
PC-centOS <meiyang@master.(none)>
2015-12-17 23:57:31 -0800

    首次提交

---
+++ activemq-runtime-config/target/generated/src/main/java/org/apache/activemq/schema/core/package-info.java
@@ -0,0 +1,9 @@
+//
+// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.2.10-b140310.1920 生成的
+// 请访问 <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// 在重新编译源模式时, 对此文件的所有修改都将丢失。
+// 生成时间: 2015.12.17 时间 03:29:05 PM CST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://activemq.apache.org/schema/core", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.apache.activemq.schema.core;
